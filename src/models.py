b4b16b3245b0b797bce50e938f2fccf91e7e9744
VaGNaroK <indialuz42@gmail.com>
2024-05-15 15:00:00 -0300

    feat: implementa a classe modelo Task para o dominio de logistica

---
+++ src/models.py
@@ -0,0 +1,14 @@
+﻿class Task:
+    def __init__(self, task_id, title, description, status="A Fazer"):
+        self.id = task_id
+        self.title = title
+        self.description = description
+        self.status = status
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "status": self.status
+        }
